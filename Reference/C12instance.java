9ae4a6bc2d3a029696cb7efc23eaffbf7a44ef11
Hwani <klh481@naver.com>
2023-08-28 15:33:54 +0900

    객체와 클래스

---
+++ Reference/C12instance.java
@@ -0,0 +1,12 @@
+package Reference;
+
+public class C12instance {
+
+    public static void main(String[] args) {
+
+        // instance = object = 객체
+        // 1. 속성( 값, 필드, property, attribute )
+        // 2. 기능( 메서드, 함수, 액션 )
+
+    }
+}
