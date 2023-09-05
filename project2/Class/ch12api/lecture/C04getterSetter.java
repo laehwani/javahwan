fae475fb3b875a0db1bbccf62cbab8d020e34da8
Hwani <klh481@naver.com>
2023-09-05 11:41:54 +0900

    9/5 getter와 setter 예제

---
+++ project2/Class/ch12api/lecture/C04getterSetter.java
@@ -0,0 +1,43 @@
+package project2.Class.ch12api.lecture;
+
+public class C04getterSetter {
+    public static void main(String[] args) {
+        MyClass04 o1 = new MyClass04();
+//        o1.name = "홍";
+//        o1.age = 33;
+        o1.setName("손흥민");
+        o1.setAge(33);
+
+        System.out.println(o1);
+        System.out.println(o1.getName()+o1.getAge());
+    }
+}
+
+class MyClass04 {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass04{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
